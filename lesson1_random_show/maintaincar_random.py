1842f51a29cc1e84fb4a546c864e1bed66946ee6
Michael Yang <michael.yang@arm.com>
2025-06-16 12:16:03 +0100

    add mountain car

---
+++ lesson1_random_show/maintaincar_random.py
@@ -0,0 +1,24 @@
+import gymnasium as gym
+import time
+
+env = gym.make("MountainCar-v0", render_mode="human")
+obs, info = env.reset()
+done = False
+
+for _ in range(1000):
+    action = env.action_space.sample()  # 随机动作：0,1,2
+    if action == 0:
+        print("向左推")
+    elif action == 1:
+        print("不动")
+    else:
+        print("向右推")    
+    
+    obs, reward, terminated, truncated, info = env.step(action)
+    print(f"Observation: {obs}, Reward: {reward}, Terminated: {terminated}, Truncated: {truncated}")
+    env.render()  
+    time.sleep(0.02)
+    if terminated or truncated:
+
+        print("Resetting...")
+        obs, info = env.reset()
